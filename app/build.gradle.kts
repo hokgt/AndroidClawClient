aea81a7ec94b3b5699a34119bbec754219e081fb
Alvin <alvin223@proton.me>
2026-02-22 17:31:02 +0700

    chore: set semantic versioning 0.7.0-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.clawtalk.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 7
+        versionName = "0.7.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
